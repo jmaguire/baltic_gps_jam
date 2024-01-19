88add6c540fd7c252b24dbddd01fb76d28c92c4a
jmaguire <jmaguire@users.noreply.github.com>
2024-01-19 15:58:00 -0800

    super_speed

--- process_ais.py
+++ process_ais.py
@@ -11,10 +11,10 @@ import logging
 
 
 BUFFER_SIZE = 12  # Generally each sample is 1m apart
-MAX_SAMPLE_DELTA_SECONDS = 120 # Ensure buffer samples are within this delta
-MIN_ALT_FEET = 10000 # Cutoff for AIS data
-GPS_THRESHOLD_LOW = .3 # GPS loss is usually ~ 0
-GPS_THRESHOLD_HIGH = 8 # GPS signal is usually > 7
+MAX_SAMPLE_DELTA_SECONDS = 120  # Ensure buffer samples are within this delta
+MIN_ALT_FEET = 10000  # Cutoff for AIS data
+GPS_THRESHOLD_LOW = .3  # GPS loss is usually ~ 0
+GPS_THRESHOLD_HIGH = 8  # GPS signal is usually > 7
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -24,6 +24,7 @@ logging.basicConfig(
     ]
 )
 
+
 def get_zero_crossings(df):
     ''' Looks at all AIS data and runs the following algorithm: '''
     ''' Read the AIS file '''
@@ -31,19 +32,19 @@ def get_zero_crossings(df):
     ''' 2. For each hex store a buffer 12 samples (collections.deque) '''
     ''' 3. Add samples and if there’s a gap of more than MAX_SAMPLE_DELTA_SECONDS flush the buffer'''
     '''     a. This ensures each sample is within MAX_SAMPLE_DELTA_SECONDS of each other'''
-    ''' 4. When the buffer is full look for a loss or recovery of signal between the first and last half of the buffer.''' 
+    ''' 4. When the buffer is full look for a loss or recovery of signal between the first and last half of the buffer.'''
     '''     a. Since there are tons of signals eventually you’ll get a nice split in the center.'''
     '''     b. Loss of signal is when a half has an average NIC < GPS_THRESHOLD_LOW.'''
     '''     c. Signal is when a half has an average >= GPS_THRESHOLD_HIGH.'''
     '''     d. Looking at the data this is usually a complete loss or recovery from a complete loss. '''
     '''5.  When we find a loss or recovery, save the middle sample.'''
-    
-    def validate_sample_interval(dequeue_list, record):
+
+    def validate_sample_interval(dequeue_list, current_timestamp):
         '''Valid sample intervals are when buffer is empty or data is within MAX_SAMPLE_DELTA_SECONDS'''
         if len(dequeue_list) == 0:
             return True
-        last_sample_timestamp = dequeue_list[-1]["timestamp"]
-        return (record["timestamp_u"] - last_sample_timestamp).total_seconds() < MAX_SAMPLE_DELTA_SECONDS
+        last_sample_timestamp = dequeue_list[-1].timestamp_u
+        return (current_timestamp - last_sample_timestamp).total_seconds() < MAX_SAMPLE_DELTA_SECONDS
 
     def detect_loss_or_recovery(buffer):
         '''Divide buffer in half and look for loss or recovery of a signal'''
@@ -63,19 +64,19 @@ def get_zero_crossings(df):
         return collections.deque(
             maxlen=BUFFER_SIZE)
 
-    def get_elems_from_buffer(buffer, key):
-        return [elem[key] for elem in buffer]
+    def get_nics_from_buffer(buffer):
+        return [elem.nic for elem in buffer]
 
     start_time = time.time()
     flight_dict = {}
     zero_crossings = []
 
-    for index, row in df.iterrows():
+    for row in df.itertuples():
         try:
-            hex_code = row["hex"]
+            hex_code = row.hex
 
             # Skip aircraft on the ground
-            altitude = row["alt_baro"]
+            altitude = row.alt_baro
             if not str(altitude).isdigit() or int(altitude) < MIN_ALT_FEET:
                 continue
 
@@ -84,44 +85,39 @@ def get_zero_crossings(df):
                 flight_dict[hex_code] = get_new_buffer()
 
             # When too much time has past, flush the buffers
-            if hex_code in flight_dict and not validate_sample_interval(flight_dict[hex_code], row):
+            if hex_code in flight_dict and not validate_sample_interval(flight_dict[hex_code], row.timestamp_u):
                 flight_dict[hex_code] = get_new_buffer()
 
             # Add new record row
-            flight_dict[hex_code].append({
-                "nic": row["nic"], "timestamp": row["timestamp_u"], "index": index
-            })
+            flight_dict[hex_code].append(row)
 
             # If buffer is full start checking for zero crossings
             if hex_code in flight_dict and len(flight_dict[hex_code]) == BUFFER_SIZE:
                 try:
-                    nics = get_elems_from_buffer(flight_dict[hex_code], "nic")
+                    nics = get_nics_from_buffer(flight_dict[hex_code])
                     # Trigger a zero crossing when gain or loss occurs
                     if detect_loss_or_recovery(nics):
-                        zero_cross_index = flight_dict[hex_code][int(
-                            BUFFER_SIZE/2)]["index"]
-                        zero_cross_record = df.iloc[zero_cross_index]
+                        zero_cross_record = flight_dict[hex_code][int(
+                            BUFFER_SIZE/2)]
                         zero_crossing = {
-                            "hex_code": zero_cross_record["hex"],
-                            "alt_baro": int(zero_cross_record["alt_baro"]),
-                            "row_index": zero_cross_index,
-                            "lat": zero_cross_record["lat"],
-                            "lon": zero_cross_record["lon"],
+                            "hex_code": zero_cross_record.hex,
+                            "alt_baro": int(zero_cross_record.alt_baro),
+                            "row_index": zero_cross_record.Index,
+                            "lat": zero_cross_record.lat,
+                            "lon": zero_cross_record.lon,
                             "nics": nics
                         }
-
-                        # timestamps = [str(elem) for elem in get_elems_from_buffer(
-                        #     flight_dict[hex_code], "timestamp")]
-                        # zero_crossing[timestamps] = timestamps
 
                         zero_crossings.append(zero_crossing)
 
                         # Flush buffer when we find a zero crossing so we don't double count
                         flight_dict[hex_code] = get_new_buffer()
                 except Exception as e:
-                    logging.exception(f"Zero crossing failure: {e}", exc_info=True)
+                    logging.exception(
+                        f"Zero crossing failure: {e}", exc_info=True)
         except Exception as e:
-            logging.exception(f"Processing records failure: {e}", exc_info=True)
+            logging.exception(
+                f"Processing records failure: {e}", exc_info=True)
 
     end_time = time.time()
     logging.info(f"Processed file: {(end_time - start_time)} seconds")
@@ -133,7 +129,7 @@ def horizontal_range(height):
     '''Get horizontal range to estimate where jammer is'''
     '''This should work because either the jammer just turned on, or just became visible'''
     '''Since the jammer has been on for weeks it's probably when it becomes visible'''
-    RADIUS_EARTH = 6371000 # Radius in meters
+    RADIUS_EARTH = 6371000  # Radius in meters
     FEET_TO_METERS = 0.3048
     height *= FEET_TO_METERS
     return np.sqrt(2*RADIUS_EARTH*height + np.power(height, 2))
@@ -141,7 +137,7 @@ def horizontal_range(height):
 
 def create_kml(zero_crossings, filename='data.kml'):
     '''Create a kml file of all of the horizontal ranges as circles centered at loss or recovery of signal'''
-    NUMBER_OF_VERTICES = 36 #36 is best practice per developer
+    NUMBER_OF_VERTICES = 36  # 36 is best practice per developer
     kml = simplekml.Kml()
     for elem in zero_crossings:
         radius = horizontal_range(elem['alt_baro'])
